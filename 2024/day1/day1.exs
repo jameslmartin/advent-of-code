62ccf8ec4f5976bb23c9f5a5f8bc8dc9a51f12ee
jameslmartin <james.martin@guildeducation.com>
2024-12-01 21:37:49 -0500

    Minor refactors

--- 2024/day1/day1.exs
+++ 2024/day1/day1.exs
@@ -5,22 +5,16 @@ defmodule Day1 do
     |> Enum.map(&Tuple.to_list/1)
   end
 
-  defp to_lists(list) do
-    Enum.map list, &(String.split(&1, " ", trim: true))
-  end
-
   def clean_input(file) do
     {:ok, input} = File.read(file)
     input
     |> String.split("\n", trim: true)
-    |> to_lists
+    |> Enum.map(&(String.split(&1, " ", trim: true)))
     |> transpose
   end
 
   def sort_diff_and_sum([l1, l2]) do
-    l1 = Enum.sort(l1)
-    l2 = Enum.sort(l2)
-    Enum.zip_with([l1, l2], fn [x, y] -> abs(String.to_integer(y) - String.to_integer(x)) end)
+    Enum.zip_with([Enum.sort(l2), Enum.sort(l1)], fn [x, y] -> abs(String.to_integer(y) - String.to_integer(x)) end)
     |> Enum.sum
   end
 
